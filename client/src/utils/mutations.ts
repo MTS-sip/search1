be55e0bb1320e18644889ddba42db1660e83da4f
M Possible <185700337+MTS-sip@users.noreply.github.com>
2025-04-22 19:37:57 -0400

    clean node modules, reinstall dep, dev

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -50,8 +50,8 @@ export const SAVE_BOOK = gql`
 
 
 export const REMOVE_BOOK = gql`
-  mutation deleteBook($bookId: ID!) {
-    deleteBook(bookId: $bookId) {
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId){
       _id
       username
       email
